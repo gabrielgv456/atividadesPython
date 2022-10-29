c7c3fd9e419122e2cdd739ecabf65a3e636d673b
gabrielgv456 <gabrielgv4569@gmail.com>
2022-10-29 11:11:17 -0300

    adicionar atividade 3

---
+++ Atividade3_MenorNumero.py
@@ -0,0 +1,7 @@
+arrayNumeros = []
+
+value1 = arrayNumeros.append(int(input('Digite o primeiro número inteiro: ')))
+value2 = arrayNumeros.append(int(input('Digite o segundo número inteiro: ')))
+value3 = arrayNumeros.append(int(input('Digite o terceiro número inteiro: ')))
+
+print(f'O número menor é o {min(arrayNumeros)}')
