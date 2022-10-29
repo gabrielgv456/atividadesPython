3817cba8eb5ef04a860cf033974f14eb1973b0e7
gabrielgv456 <gabrielgv4569@gmail.com>
2022-10-28 23:28:08 -0300

    First Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import math
+
+def calcDates(daysYearsOld):
+    years  = daysYearsOld / 365
+    months = (daysYearsOld % 365) / 30
+    days   = (daysYearsOld % 365)  % 30
+    return math.floor(years), math.floor(months), days
+
+
+daysYearsOld = input('Digite a sua idade expressa em dias: ')
+resultYears, resultMonths, resultDays = calcDates(int(daysYearsOld))
+print(f'Você possui {resultYears} anos, {resultMonths} meses e {resultDays} dias de vida')
+
+
+
